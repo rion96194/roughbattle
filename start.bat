28646087a089d50248006b80aae3cbcbdc4273ee
rion96194 <rion_96194_y@yahoo.co.jp>
2021-02-08 11:58:05 +0900

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-echo "# roughbattle" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rion96194/roughbattle.git
-git push -u origin main
-pause
